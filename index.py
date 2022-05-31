a538e39fbca63865fc26c6e04861bbb5af9ee7e1
Kevin <kevinstopper1@gmail.com>
2022-05-31 08:25:57 -0500

    Commit Html

--- index.py
+++ index.py
@@ -3,13 +3,22 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+#ruta principal en la que tendremos nuestra pagina de inicio de sesion
 @app.route('/')
 def helloworld():
     return render_template("index.html")
 
+@app.route('/registro.html/')
+def registro():
+    return render_template("registro.html")
+
 @app.route('/tienda.html/')
-def helloworld():
-    return render_template("index.html")
+def tienda():
+    return render_template("tienda.html")
+
+@app.route('/Admin.html/')
+def Admin():
+    return render_template("Admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
