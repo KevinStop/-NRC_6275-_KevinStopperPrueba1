0379e8eb7b919208295dad172fae5701567b6477
Kevin <kevinstopper1@gmail.com>
2022-05-31 08:49:05 -0500

    Commit Final

--- index.py
+++ index.py
@@ -8,16 +8,22 @@ app = Flask(__name__)
 def helloworld():
     return render_template("index.html")
 
+#Ruta de a pagina registro
 @app.route('/registro.html/')
 def registro():
+    #Retornamos Template registro
     return render_template("registro.html")
 
+#Ruta de la pagina tienda
 @app.route('/tienda.html/')
 def tienda():
+    #Retornamos Template tienda
     return render_template("tienda.html")
 
+#Ruta de la pagina Admin
 @app.route('/Admin.html/')
 def Admin():
+    #Retornamos Template Admin
     return render_template("Admin.html")
 
 if __name__ == "__main__":
